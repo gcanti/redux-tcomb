12f94c2960eca76766374a2e31cfcfbbc8b04e3f
gcanti <giulio.canti@gmail.com>
2015-09-05 09:21:29 +0200

    create Action once

--- examples/todomvc/createReducer.js
+++ examples/todomvc/createReducer.js
@@ -2,7 +2,8 @@ import State from './State';
 import * as actions from './actions';
 import { createUnion, createReducer } from '../../lib';
 
+const Action = createUnion(actions);
+
 export default function (initialState) {
-  const Action = createUnion(actions);
   return createReducer(State(initialState), Action, State);
 }
